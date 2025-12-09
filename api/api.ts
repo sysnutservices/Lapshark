73f5c59660d0de9992977ff2de25ae6ce79636c4
Your Name <your@email.com>
2025-12-09 15:41:15 +0530

    Initial commit

--- api/api.ts
+++ api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-export const API_URL = "https://express-opal-six.vercel.app/api";
-export const API_URL2 = "https://express-opal-six.vercel.app";
+export const API_URL = "https://prorevision-desirously-omar.ngrok-free.dev/api";
+export const API_URL2 = "https://prorevision-desirously-omar.ngrok-free.dev";
 // export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
 // export const API_URL2 = process.env.NEXT_PUBLIC_API_URL_ROOT || "http://localhost:5000";
 export const api = axios.create({
